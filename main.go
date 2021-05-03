87e56816e41dd0adae7cd9350dbc73b8d24378f4
yoshikouki <yoshikouki@pepabo.com>
2021-05-03 12:36:37 +0900

    Import "github.com/google/go-cmp/cmp" in code

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/yoshikouki/go-sample/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main()  {
 	fmt.Println(morestrings.ReverseRunes("Hello, world."))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
