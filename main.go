e394563e4dbd6d3825fbdffa2b899f3d29f2a89b
yoshikouki <yoshikouki@pepabo.com>
2021-05-03 12:26:37 +0900

    Import morestrings

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/yoshikouki/go-sample/morestrings"
+)
 
 func main()  {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("Hello, world."))
 }
